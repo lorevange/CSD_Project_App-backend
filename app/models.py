6e1483d14661851d5308a67bb9363fa2f2bab979
lorevange <lorenzo.evangelisti2@gmail.com>
2025-11-22 13:00:15 +0100

    Fix table name User

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from .database import Base
 
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "User"
 
     identity_number = Column(String, primary_key=True, index=True, unique=True)
     first_name = Column(String, nullable=False)
