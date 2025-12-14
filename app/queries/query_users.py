9fb720e83d06e7c87eeb8f41161abe8fe9d678c7
Lorenzo Evangelisti <lorenzo.evangelisti2@gmail.com>
2025-12-14 12:22:39 +0100

    Already existing email user registration error

--- app/queries/query_users.py
+++ app/queries/query_users.py
@@ -13,3 +13,11 @@ def get_user_by_email_and_password(email: str, password: str, db: Session) -> mo
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     return user
+
+def get_user_by_email(email: str, db: Session) -> models.User:
+    user = (
+        db.query(models.User)
+        .filter(models.User.email == email)
+        .first()
+    )
+    return user
--- app/services/service_user.py
+++ app/services/service_user.py
@@ -1,10 +1,15 @@
 from sqlalchemy.orm import Session
+from fastapi import HTTPException
 
 from app import models, schemas
+from ..queries.query_users import get_user_by_email
 
 
 def create_user(base: schemas.UserCreateBase, profile: str, db: Session) -> models.User:
     """Create and persist a base User row."""
+    if get_user_by_email(base.email, db):
+        raise HTTPException(status_code=404, detail="There is already a user associated to this email.")
+    
     user = models.User(
         first_name=base.first_name,
         last_name=base.last_name,
